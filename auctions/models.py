8e486427b24d8c8063ff0f8fb02bbd3e22bb15af
Martyanov Andrey <37772440+martyanovandrey@users.noreply.github.com>
2020-09-22 06:59:25 +0300

    auctions

---
+++ auctions/models.py
@@ -0,0 +1,25 @@
+from django.contrib.auth.models import AbstractUser
+from django.db import models
+
+
+class User(AbstractUser):
+    pass
+
+class Listings(models.Model):
+    name = models.CharField(max_length=64)
+    price = models.DecimalField(max_digits=16, decimal_places=2)
+    description = models.CharField(max_length=254)
+    date = models.DateTimeField()
+
+    def __str__(self):
+        return f"{self.name} ({self.price}) {self.description} {self.date}"    
+
+class Comments(models.Model):
+    name = models.CharField(max_length=64)
+    comment = models.CharField(max_length=1024)
+    date = models.DateTimeField()    
+
+    def __str__(self):
+        return f"{self.name} ({self.comment}) {self.date}"      
+
+
