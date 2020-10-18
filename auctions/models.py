e5fb9259eae6db837e5b806680e0beac1e9c7c3c
Martyanov Andrey <37772440+martyanovandrey@users.noreply.github.com>
2020-10-18 12:50:12 +0300

    fix categories

--- auctions/models.py
+++ auctions/models.py
@@ -11,7 +11,7 @@ class Listing(models.Model):
     category = models.CharField(max_length=64, blank=True)
     url = models.CharField(max_length=254)
     date = models.DateTimeField(auto_now_add=True, blank=True)
-    owner = models.CharField(max_length=64)
+    owner = models.ForeignKey(User, on_delete=models.CASCADE, related_name='owner')
     active = models.BooleanField(default=True)
     winner = models.ForeignKey(User, on_delete=models.CASCADE, related_name='winner', null=True, blank=True)
 
